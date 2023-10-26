6c54c8d1f09e08a7763a373a61d50a07aae2a01c
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-26 09:16:44 -0400

    Add example function using imported data - test - test with renamed function call dbh - test with ... indeterminate args Assign test with ... to an object

--- 02_Loops_and_functions/Elin_functions&loops.R
+++ 02_Loops_and_functions/Elin_functions&loops.R
@@ -10,9 +10,29 @@ trees_mlunguya <- read.csv("02_Loops_and_functions/trees_mlunguya.csv")
 head(trees_bicuar)
 str(trees_bicuar)
 
+
 example.fn <- function(x, y){
   # Perform an action using x and y
   x + y
 }
 
 example.fn(x = 1, y = 2)
+
+
+basal.area <- function(x){
+  (pi*(x)^2)/40000
+}
+
+basal.area <- function(dbh){
+  (pi*(dbh)^2)/40000
+}
+
+basal.area(dbh = trees_bicuar$diam)
+
+basal.area <- function(...){
+  (pi*c(...)^2)/40000
+}
+
+basal.area(trees_bicuar$diam, trees_mlunguya$diam)
+
+trees_bicuar$ba <- basal.area(dbh = trees_bicuar$diam)
