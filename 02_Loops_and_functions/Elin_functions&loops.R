425a6df0282b52b201f4c5257d66cc0fbd44b39e
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-26 08:20:34 -0400

    Create CC stream 3 master R project Correct file path placements create R script for 02-functions-and-loops

---
+++ 02_Loops_and_functions/Elin_functions&loops.R
@@ -0,0 +1,16 @@
+# Functions and Loops
+# By: Elin Swank
+# 10/26/2023
+# CC-5
+###########################
+
+trees_bicuar <- read.csv("02_Loops_and_functions/trees_bicuar.csv")
+trees_mlunguya <- read.csv("02_Loops_and_functions/trees_mlunguya.csv")
+
+head(trees_bicuar)
+str(trees_bicuar)
+
+example.fn <- function(x, y){
+  # Perform an action using x and y
+  x + y
+}
