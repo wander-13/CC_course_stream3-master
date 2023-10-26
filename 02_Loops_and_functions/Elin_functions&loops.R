b609f8473290041a3697dd6de28b755e405bf85c
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-26 10:25:05 -0400

    Import csv data to object Create a scatter plot of data subset Troubleshooting issue running lines 127-150 - attach dplyr library    - Fixed issue

--- 02_Loops_and_functions/Elin_functions&loops.R
+++ 02_Loops_and_functions/Elin_functions&loops.R
@@ -4,6 +4,9 @@
 # CC-5
 ###########################
 
+library(ggplot2)
+library(dplyr)
+
 trees_bicuar <- read.csv("02_Loops_and_functions/trees_bicuar.csv")
 trees_mlunguya <- read.csv("02_Loops_and_functions/trees_mlunguya.csv")
 
@@ -116,3 +119,32 @@ diam.summ <- function(dbh, mean = TRUE, median = TRUE, ba = TRUE){
 }
 
 diam.summ(dbh = trees_bicuar$diam, mean = TRUE, median = T)
+
+
+LPI <- read.csv("02_Loops_and_functions/LPI_data_loops.csv")
+
+# Scatteplot to examine how Griffon populations have changed between 1970 and 2014 in Croatia and Italy
+vulture <- filter(LPI, Common.Name == "Griffon vulture / Eurasian griffon")
+vultureITCR <- filter(vulture, Country.list == c("Croatia", "Italy"))
+
+(vulture_scatter <- ggplot(vultureITCR, aes(x = year, y = abundance, colour = Country.list)) +
+    geom_point(size = 2) +                                              # Changing point size
+    geom_smooth(method = lm, aes(fill = Country.list)) +                # Adding a linear model fit and colour-coding by country
+    scale_fill_manual(values = c("#EE7600", "#00868B")) +               # Adding custom colours
+    scale_colour_manual(values = c("#EE7600", "#00868B"),               # Adding custom colours
+                        labels = c("Croatia", "Italy")) +               # Adding labels for the legend
+    ylab("Griffon vulture abundance\n") +                             
+    xlab("\nYear")  +
+    theme_bw() +
+    theme(axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust = 1),       # making the years at a bit of an angle
+          axis.text.y = element_text(size = 12),
+          axis.title.x = element_text(size = 14, face = "plain"),             
+          axis.title.y = element_text(size = 14, face = "plain"),             
+          panel.grid.major.x = element_blank(),                                # Removing the background grid lines                
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_blank(),  
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),           # Adding a 0.5cm margin around the plot
+          legend.text = element_text(size = 12, face = "italic"),              # Setting the font for the legend text
+          legend.title = element_blank(),                                      # Removing the legend title
+          legend.position = c(0.9, 0.9)))               # Setting the position for the legend - 0 is left/bottom, 1 is top/right
