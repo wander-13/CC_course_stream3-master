90f1264dfc3683ed3b1aba968303a322768e1406
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-27 08:05:39 -0400

    Load data and check Peform NMDS and plot Perform PCA and plot relative eigenvaluse Calculate % variance explained by first 2 axes Plot PCA

--- 04_Ordination/Elin_ordination_tutorial.R
+++ 04_Ordination/Elin_ordination_tutorial.R
@@ -15,3 +15,34 @@ install.packages("dplyr")
 library(vegan)
 library(ape)
 library(dplyr)
+
+# Load the community dataset which we`ll use in the examples today
+data(varespec)
+
+# Open the dataset and look if you can find any patterns
+View(varespec)
+# It is probably very difficult to see any patterns by just looking at the data frame!
+
+# With this command, you`ll perform a NMDS and plot the results
+varespec %>%
+  metaMDS(trace = F) %>%
+  ordiplot(type = "none") %>%
+  text("sites")
+
+PCA <- rda(varespec, scale = FALSE)
+# Use scale = TRUE if your variables are on different scales (e.g. for abiotic variables).
+# Here, all species are measured on the same scale 
+# So use scale = FALSE
+
+# Now plot a bar plot of relative eigenvalues. This is the percentage variance explained by each axis
+barplot(as.vector(PCA$CA$eig)/sum(PCA$CA$eig)) 
+# How much of the variance in our dataset is explained by the first principal component?
+
+# Calculate the percent of variance explained by first two axes
+sum((as.vector(PCA$CA$eig)/sum(PCA$CA$eig))[1:2]) # 79%, this is ok.
+# Also try to do it for the first three axes
+
+# Now, we`ll plot our results with the plot function
+plot(PCA)
+plot(PCA, display = "sites", type = "points")
+plot(PCA, display = "species", type = "text")
