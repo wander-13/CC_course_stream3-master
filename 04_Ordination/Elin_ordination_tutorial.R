4cc903ecaf8e9c0de76ab6e74e05e70fa3015b54
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-27 07:24:06 -0400

    Create ordination R script Set working directory Install necesary packages Load packages

---
+++ 04_Ordination/Elin_ordination_tutorial.R
@@ -0,0 +1,17 @@
+# Ordination
+# By: Elin Swank
+# Date: 10/27/2023
+# Coding Club Tutorial
+################################################################################
+
+# Set the working directory (if you didn`t do this already)
+# setwd("your_filepath")
+
+# Install and load the following packages
+install.packages("vegan")
+install.packages("ape")
+install.packages("dplyr")
+
+library(vegan)
+library(ape)
+library(dplyr)
