6c89a8d0e4a0f8b18a39fe0a81abb774b550d16d
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-10-27 10:04:25 -0400

    Complete PCoA practice Calc. dist. matrix Initial NMDS- choose # of dimensions NMDS on original community matrix - specified distance matrix - plot sites and species Correlating environmental variables - envfit() to add env.variables as vectors to ordination plot - plot var. with significant p values Classification overlay - creates class scheme - apply using for() loop - plot Challenges- use dune data set - perform ordination - perform classification - plot

--- 04_Ordination/Elin_ordination_tutorial.R
+++ 04_Ordination/Elin_ordination_tutorial.R
@@ -94,3 +94,138 @@ biplot.pcoa(PCOA)
 # Hence, no species scores could be calculated. 
 #However, we could work around this problem like this:
 biplot.pcoa(PCOA, varespec)
+
+# Extract the plot scores from first two PCoA axes (if you need them):
+PCOAaxes <- PCOA$vectors[,c(1,2)]
+
+# Compare this result with the PCA plot
+par(mfrow = c(1, 2)) 
+biplot.pcoa(PCOA)
+plot(PCA)
+
+# reset plot window
+par(mfrow = c(1, 1)) 
+
+# First step is to calculate a distance matrix. See PCOA for more information about the distance measures
+# Here we use bray-curtis distance, which is recommended for abundance data
+dist <- vegdist(varespec,  method = "bray")
+
+# In this part, we define a function NMDS.scree() that automatically 
+# performs a NMDS for 1-10 dimensions and plots the nr of dimensions vs the stress
+NMDS.scree <- function(x) { #where x is the name of the data frame variable
+  plot(rep(1, 10), replicate(10, metaMDS(x, autotransform = F, k = 1)$stress), xlim = c(1, 10),ylim = c(0, 0.30), xlab = "# of Dimensions", ylab = "Stress", main = "NMDS stress plot")
+  for (i in 1:10) {
+    points(rep(i + 1,10),replicate(10, metaMDS(x, autotransform = F, k = i + 1)$stress))
+  }
+}
+
+# Use the function that we just defined to choose the optimal nr of dimensions
+NMDS.scree(dist)
+
+# Because the final result depends on the initial 
+# random placement of the points 
+# we`ll set a seed to make the results reproducible
+set.seed(2)
+
+# Here, we perform the final analysis and check the result
+NMDS1 <- metaMDS(dist, k = 2, trymax = 100, trace = F)
+# Do you know what the trymax = 100 and trace = F means?
+# Let's check the results
+NMDS1
+
+# If you don`t provide a dissimilarity matrix, metaMDS automatically applies Bray-Curtis. So in our case, the results would have to be the same
+# NMDS2 <- metaMDS(varespec, k = 2, trymax = 100, trace = F)
+# NMDS2
+# turn off auto transform
+NMDS2 <- metaMDS(varespec, k = 2, trymax = 100, trace = F, autotransform = F)
+NMDS2
+
+stressplot(NMDS1)
+plot(NMDS1, type = "t") # no species scores
+# give metaMDS() orginal community matrix as input and specify distance matrix
+NMDS3 <- metaMDS(varespec, k = 2, trymax = 100, trace = F, autotransform = FALSE, distance="bray")
+plot(NMDS3)
+plot(NMDS3, display = "sites", type = "n")
+points(NMDS3, display = "sites", col = "red", cex = 1.25)
+text(NMDS3, display ="species")
+
+# Alternatively, you can use the functions ordiplot and orditorp
+ordiplot(NMDS3, type = "n")
+orditorp(NMDS3, display = "species", col = "red", air = 0.01)
+orditorp(NMDS3, display = "sites", cex = 1.1, air = 0.01)
+
+# correlating environmental variables with our ordination axes
+# Load the second dataset
+data(varechem)
+
+# The function envfit will add the environmental variables as vectors to the ordination plot
+ef <- envfit(NMDS3, varechem, permu = 999)
+ef
+
+# The two last columns are of interest: the squared correlation coefficient and the associated p-value
+# Plot the vectors of the significant correlations and interpret the plot
+plot(NMDS3, type = "t", display = "sites")
+plot(ef, p.max = 0.05)
+
+# displaying groups on ordination plots
+# Define a group variable (first 12 samples belong to group 1, last 12 samples to group 2)
+group = c(rep("Group1", 12), rep("Group2", 12))
+
+# Create a vector of color values with same length as the vector of group values
+colors = c(rep("red", 12), rep("blue", 12))
+
+# Plot convex hulls with colors based on the group identity
+ordiplot(NMDS3, type = "n")
+for(i in unique(group)) {
+  ordihull(NMDS3$point[grep(i, group),], draw="polygon",
+           groups = group[group == i],col = colors[grep(i,group)],label=F) } 
+
+orditorp(NMDS3, display = "species", col = "red", air = 0.01)
+orditorp(NMDS3, display = "sites", col = c(rep("red",12),
+                                           rep("blue", 12)), air = 0.01, cex = 1.25)
+
+# Challenge 1
+# ordinaition on Dune data set
+data(dune)
+
+dunedist <- vegdist(dune,  method = "bray")
+
+NMDS.scree(dunedist)
+
+duneNMDS1 <- metaMDS(dunedist, k = 2, trymax = 100, trace = F)
+duneNMDS1
+
+duneNMDS2 <- metaMDS(dune, k = 2, trymax = 100, trace = F, autotransform = F)
+duneNMDS2
+
+data("dune.env")
+
+duneNMDS3 <- metaMDS(dune, k = 2, trymax = 100, trace = F, autotransform = F, distance = "bray")
+
+plot(duneNMDS3)
+
+plot(duneNMDS3, display = "sites", type = "n")
+
+points(duneNMDS3, display = "sites", col = "red", cex = 1.25)
+text(duneNMDS3, display ="species")
+
+# The function envfit will add the environmental variables as vectors to the ordination plot
+efdune <- envfit(duneNMDS3, dune.env, permu = 999)
+efdune
+
+plot(duneNMDS3, type = "t", display = "sites")
+plot(efdune, p.max = 0.05)
+
+# Challenge 2
+# perform classification and overlay on ordination
+group = c(rep("Group1", 12), rep("Group2", 8))
+colors = c(rep("red", 12), rep("blue", 8))
+
+ordiplot(duneNMDS3, type = "n")
+for(i in unique(group)) {
+  ordihull(duneNMDS3$point[grep(i, group),], draw="polygon",
+           groups = group[group == i],col = colors[grep(i,group)],label=F) } 
+
+orditorp(duneNMDS3, display = "species", col = "red", air = 0.01)
+orditorp(duneNMDS3, display = "sites", col = c(rep("red",12),
+                                           rep("blue", 12)), air = 0.01, cex = 1.25)
