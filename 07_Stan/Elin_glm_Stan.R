31e3097dd99ca9eae76391d3690a50e28a1dcdce
wander-13 <97532804+wander-13@users.noreply.github.com>
2023-11-02 08:32:11 -0400

    Change variable type Check data distribution Enable Parallel computing on multiple cores Example Stan code for model, plot, and summary Check years in data Note about changing year format and why Run stan glm model with year transform inside code

--- 07_Stan/Elin_glm_Stan.R
+++ 07_Stan/Elin_glm_Stan.R
@@ -21,3 +21,44 @@ toolik_richness <- read.csv("07_Stan/toolik_richness.csv")
 
 # Inspect data
 head(toolik_richness)
+
+str(toolik_richness)
+
+# change variable type to reflect reality
+toolik_richness$Plot <- as.factor(as.character(toolik_richness$Plot))
+
+# explore distributions
+(hist <- ggplot(toolik_richness, aes(x = Richness)) +
+    geom_histogram() +
+    theme_classic())
+### data are right-skewed, poisson distribution may be suitable for the model
+
+# enabe parallel computing between cores
+options(mc.cores = parallel::detectCores())
+
+# Example Stan code for model production
+# Note - this code could take hours to run!
+# We are running the model using the default weakly informative priors.
+# More about priors coming later in the tutorial!
+# stan_lm <- stan_glmer(Richness ~ I(Year-2007) + (1|Site/Block/Plot),
+#                     data = toolik_richness, family = poisson,
+#                     chains = 4, cores = 4)
+
+# Assess converge by looking at the trace plots
+# plot(stan_lm, plotfun = "trace")
+
+# Explore the summary output
+# summary(stan_lm)
+
+# check years of data
+unique(toolik_richness$Year)
+
+# for modeling purposes: so that the model doesn't estimate richness for 2,007 years
+# but starts estimates at the first year of monitoring
+# transform the year into a continuous variable starting at one
+
+# Note how now we are using stan_glm because
+# there are no random effects
+stan_glm1 <- stan_glm(Richness ~ I(Year-2007),  # year transformation
+                      data = toolik_richness, family = poisson,
+                      chains = 4, cores = 4)
